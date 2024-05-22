b835a76e623bcbc41de2c3fb72716a8c912df250
Mark8904 <168806203+Mark8904@users.noreply.github.com>
2024-05-22 06:42:14 +0530

    System_monitor.sh

--- OS/#!/bin/System_monitor.sh
+++ OS/#!/bin/System_monitor.sh
@@ -35,3 +35,20 @@ echo -e "\e[31;43m***** SYSTEM UPTIME AND LOAD *****\e[0m"
 uptime
 
 echo ""
+# -Logged-in users:
+
+echo -e "\e[31;43m***** CURRENTLY LOGGED-IN USERS
+
+*****\e[0m" who
+
+echo ""
+
+# -Top 5 processes as far as memory usage is concerned
+
+echo -e "\e[31;43m***** TOP 5 MEMORY-CONSUMING PROCESSES
+
+*****\e[0m" ps -eo %mem,%cpu,comm --sort=-%mem | head -n 6
+
+echo ""
+
+echo -e "\e[1;32mDone.\e[0m"
